7e0c52d1f28e441657e875b6a57115f3cc643d64
Matthias Yang He <myh7@psu.edu>
2022-09-23 15:47:22 -0400

    Update conf.py to fix module import errors for ReadtheDocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,6 +3,12 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../src'))  # Source code dir relative to this file
+
+
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -19,12 +25,24 @@ extensions = [
    'sphinx.ext.doctest',
    'sphinx.ext.autodoc',
    'sphinx.ext.autosummary',
+   'sphinx.ext.mathjax',
+   'sphinx.ext.napoleon',
+   'sphinx_toolbox.installation',
+   'sphinx_toolbox.collapse',
 ]
+autosummary_generate = True
+add_module_names = False # whether to prepend module names to functions/objects
+autodoc_member_order = 'bysource' # sort docs for members by the order in which they appear in the module; default is 'alphabetical'
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-autodoc_mock_imports = ["numpy"]
+autodoc_mock_imports = [
+   "numpy",
+   "matplotlib",
+   "scipy",
+   "corner",
+]
 
 
 
